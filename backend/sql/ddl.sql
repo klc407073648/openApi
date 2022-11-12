585eec5f54d369c10d3e66a19d102cfaddd9da68
klc407073648 <407073648@qq.com>
2022-11-12 22:01:16 +0800

    后端文件内容修正

--- backend/sql/ddl.sql
+++ backend/sql/ddl.sql
@@ -14,6 +14,8 @@ create table if not exists user
     gender       tinyint                                null comment '性别',
     userRole     varchar(256) default 'user'            not null comment '用户角色：user / admin',
     userPassword varchar(512)                           not null comment '密码',
+    accessKey    varchar(512)                           not null comment 'accessKey',
+    secretKey    varchar(512)                           not null comment 'secretKey',
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
