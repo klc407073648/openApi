4b877f83b3a0f84fc3423d05764cde140af570e9
klc407073648 <407073648@qq.com>
2023-04-08 10:08:06 +0800

    代码错误修正，配置修改

--- backend/src/test/java/com/klc/openapi/service/UserInterfaceInfoServiceTest.java
+++ backend/src/test/java/com/klc/openapi/service/UserInterfaceInfoServiceTest.java
@@ -1,6 +1,5 @@
 package com.klc.openapi.service;
 
-import com.klc.openapi.model.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- backend/src/test/java/com/klc/openapi/service/UserServiceTest.java
+++ backend/src/test/java/com/klc/openapi/service/UserServiceTest.java
@@ -1,6 +1,6 @@
 package com.klc.openapi.service;
 
-import com.klc.openapi.model.entity.User;
+import com.klc.openapicommon.model.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
